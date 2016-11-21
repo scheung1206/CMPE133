c84f0b0c6ed6c4993c2e5b5c1d7be6415dca4dda
Stephen Cheung <stephencheung@Stephens-MacBook-Pro.local>
2016-11-20 23:31:10 -0800

    Connections & Company Merge

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,6 @@
 class Company < ActiveRecord::Base
+  belongs_to :user
   mount_uploader :avatar, AvatarUploader
+  validates :name, :description, uniqueness: { scope: :name, message: "company already exists!" }
+  validates :name, :description, presence: true
 end
